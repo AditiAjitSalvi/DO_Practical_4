53c9793014321eb1b88cd12db20b2cb775945207
AditiSalvi <113448674+AditiAjitSalvi@users.noreply.github.com>
2025-04-17 23:59:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// index.js
-
 const express = require('express');
 const app = express();
-const port = 3000;
 
-// Define a simple route
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
 });
