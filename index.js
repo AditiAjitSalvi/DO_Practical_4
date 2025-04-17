ef7fb100c86495403fccf7fa86d16e110b55d2cf
AditiSalvi <113448674+AditiAjitSalvi@users.noreply.github.com>
2025-04-18 00:03:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+// Listen on port 5000, update console log message accordingly
 app.listen(5000, () => {
-    console.log('Server is running on http://localhost:3000');
+    console.log('Server is running on http://localhost:5000');
 });
